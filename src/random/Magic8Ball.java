a76e75883c2052986cc5e2101da721a08b6670e5
codeclub <codeclub@smms-rm23-34.smusd.local>
2019-05-01 14:55:56 -0700

    smile

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -1,35 +1,42 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
-
-
 package random;
-
 import java.util.Random;
 
+import javax.swing.JOptionPane;
 public class Magic8Ball {
-
-	// 1. Make a main method that includes all the steps below….
-
+public static void main(String[] args) {	
+	// 1. Make a main method that includes all the steps below….	
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+    int rebmun = new Random().nextInt(4);
 	// 3. Print out this variable
-
+    System.out.println(rebmun);
 	// 4. Get the user to enter a question for the 8 ball
-
+    JOptionPane.showInputDialog("Ask me ANY yes or no question?");
 	// 5. If the random number is 0
-
+    if(rebmun == 0) {
+    JOptionPane.showMessageDialog(null, "Yes");	
+    }
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
-
+    if(rebmun == 1) {
+    JOptionPane.showMessageDialog(null, "No");
+    }
 	// -- tell the user "No"
-
+    if(rebmun == 2) {
+    JOptionPane.showMessageDialog(null, "Maybe you should ask google?");
+    }
 	// 7. If the random number is 2
 
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 8. If the random number is 3
-
+    if(rebmun == 3) {
+    JOptionPane.showMessageDialog(null, "You might have to ask the gods for that one.");	
+    }
 	// -- write your own answer
 
+}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -17,9 +17,27 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+		for (int i = 0; i < 10; i++) {
 
-		// 2. Repeat all the code above 10 times
+		if(randomNumber == 0) {
+		JOptionPane.showMessageDialog(null, "You are a nice person.");	
+		}
+		if(randomNumber == 1) {		
+			JOptionPane.showMessageDialog(null, "Your shoes look nice.");
+		}
+		if(randomNumber == 2) {
+		JOptionPane.showMessageDialog(null, "You are not ugly.");
+		}
+		if(randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "You look young.");	
+		}
+		if(randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "You look beautiful.");
+		}
+			// 2. Repeat all the code above 10 times
+		
 		
 		// 3. Find someone to test out your program. They will like it :)
-	}
+		}
+		}
 }
